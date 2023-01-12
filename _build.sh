95079c4db7adbd0b99ea30c44d1af6eaab326f56
Hua Zou <zouhua1@outlook.com>
2023-01-12 15:48:53 +0800

    Update the book

--- _build.sh
+++ _build.sh
@@ -6,3 +6,6 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
+# bookdown::render_book('index.Rmd', 'bookdown::gitbook')
+# bookdown::render_book('index.Rmd', 'bookdown::pdf_book')
+# bookdown::render_book('index.Rmd', 'bookdown::epub_book')
